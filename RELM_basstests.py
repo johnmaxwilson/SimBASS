b164675f49a11932cb87c86104b5df4be2783611
johnmaxwilson <johnmaxwilson@gmail.com>
2015-03-03 19:08:54 -0800

    3/3/15

--- RELM_basstests.py
+++ RELM_basstests.py
@@ -3,6 +3,10 @@
 Created on Mon Mar  2 11:27:37 2015
 
 @author: jmwilson
+
+    Test of the mechanisms to quickly (numpy) assign each bin in a grid an 
+earthquake rate based on Omori behavior for a list of quake epicenters.
+    Does not use real lat/lons. Uses single magnitude and strike for all events
 """
 
 import math
@@ -11,67 +15,58 @@ import matplotlib.pyplot as plt
 import time
 from mpl_toolkits.basemap import Basemap
 
-sidesize = 200
-hist_grid = np.zeros((sidesize, sidesize))
-num = 1
-
-abratio = 2
-strike = 30
-angledeg = -strike
-anglerad = math.pi/180.0*angledeg
-
-mag=7.0
-q=1.5
-delm=1.0
-mc=5.5
-b=1.0
-lam=1.76
-
-centers = [(sidesize/2+0.3, sidesize/2+0.3)]#np.random.random_sample((num, 2))*sidesize#np.random.randint(0, sidesize,(num,2))#
-binlocs = np.array([[(x,y) for x in xrange(sidesize)] for y in xrange(sidesize)])
-
-
-for center in centers:
-    diff = binlocs - center
-    transform = [[((coord[0]*math.cos(anglerad)+coord[1]*math.sin(anglerad))*abratio/1,( -coord[0]*math.sin(anglerad)+coord[1]*math.cos(anglerad))/1) for coord in _] for _ in diff]
-    rad = np.linalg.norm(transform, axis=2)
-    #dist = 1.0/(2*np.pi*10**2)*np.exp(-(rad**2)/(2.0*10**2))
+def omori_el(quakes_f, binposit, size, abratio=2, strike=30, q=1.5, mag = 7.0, delm=1.0, mc=5.5, b=1.0, lam=1.76):
+    flt = float
+    convert = deg2km
+    normalize = np.linalg.norm
+    cosine = np.cos
+    sine = np.sin
     
+    histog = np.zeros((len(binposit), len(binposit[0])))
     
-    Nom = 10**(b*(mag-delm-mc))    
-    r0 = 0.5*10**(0.5*mag-lam)
-    chi = r0**(1-q)/Nom/(q-1)
-    dist = 1/chi*(r0 + rad)**-q/(2*pi*rad)
+    angledeg = -strike
+    anglerad = math.pi/180.0*angledeg
     
-    #dist = Nom/np.sum(dist)*dist
-    dist = np.ma.    
+    for quakeloc in quakes_f:
+        
+        diff = binposit - quakeloc
+        diff = [[convert(pairs) for pairs in _] for _ in diff]
+                
+        transform = [[((coord[0]*cosine(anglerad)+coord[1]*sine(anglerad))*abratio/1,( -coord[0]*sine(anglerad)+coord[1]*cosine(anglerad))/1) for coord in _] for _ in diff]        
+                
+        rad = normalize(transform, axis=2)
+        
+        Nom = 10**(b*(mag-delm-mc))    
+        r0 = 0.5*10**(0.5*mag-lam)
+        chi = r0**(1-q)/Nom/(q-1)
+        dist = 1.0/chi/(r0 + rad)**q#/(2*pi*rad)
+        
+        #dist = Nom/np.sum(dist)*dist
+        #dist = np.ma.
+        
+        histog += dist
     
-    hist_grid += dist
-    print Nom
+    return histog
+
+
+def deg2km(degs):
+    rE = 6371.0
+    latkm = math.pi/180.0*rE*degs[0]
+    lonkm = math.pi/180.0*rE*np.cos(math.pi/180.0*degs[0])*degs[1]
+    kms = (latkm, lonkm)
+    return kms
+
+
+gridsize = 200
+numquakes = 1
+
+quakes = [(gridsize/2+0.3, gridsize/2+0.3)]#np.random.random_sample((numquakes, 2))*gridsize#np.random.randint(0, gridsize,(numquakes,2))#
+binlocs = np.array([[(x,y) for x in xrange(gridsize)] for y in xrange(gridsize)])
+
+hist_grid = omori_el(quakes, binlocs, gridsize)
 
 plt.figure()
 #plt.imshow(hist_grid, interpolation='nearest')
 plt.pcolor(hist_grid)
 plt.colorbar()
-plt.show()
-print np.sum(hist_grid)
-
-#==============================================================================
-# mag=9.0
-# q=1.5
-# delm=1.0
-# mc=4.5
-# b=1.0
-# lam=1.76
-# r0 = 0.5*10**(0.5*mag-lam)
-# Nom = 10**(b*(mag-delm-mc))
-# 
-# rad = np.arange(1,100,0.1)
-# dist = (r0 + rad)**-q/rad
-# dist = Nom/np.sum(dist)*dist
-# 
-# plt.figure()
-# plt.loglog(rad, dist)
-#==============================================================================
-
 plt.show()
--- RELM_omoribins.py
+++ RELM_omoribins.py
@@ -3,9 +3,13 @@
 Created on Tue Feb 17 12:17:06 2015
 
 @author: jmwilson
+
+    Script to assign Omori-like earthquake halos in bins around earthquakes.
+Also has function for populating a dictionary of quake bin coords
 """
 #==============================================================================
-# TODO: Explicitly convert from km to lat/lon degrees
+# TODO: -Include ellipsoidal work from Mark's BASScast and RELM_basstests
+#       -Try to use masked arrays to restrict calculations to RELM test region
 #==============================================================================
 
 import numpy as np
@@ -14,21 +18,22 @@ import pickle
 import time
 
 #==============================================================================
-# Using nearest 100th degree to bin events
-#   - lat and lon are kept as int(coord*100)
+# Using nearest 10th degree to bin events
+#   - lat and lon are kept as int(coord*10)
 #==============================================================================
 
 
 def omori(quakes_f, binposit, histog, q=1.5, delm=1.0, mc=5.5, b=1.0, lam=1.76):
     flt = float
+    convert = deg2km
     normalize = np.linalg.norm
             
     for quake in quakes_f:
-        quakeloc = (flt(quake['hypo_lat'])*100, flt(quake['hypo_lon'])*100)
+        quakeloc = (flt(quake['hypo_lat'])*10, flt(quake['hypo_lon'])*10)
         mag = flt(quake['magnitude'])
-#==============================================================================
-#         diffkm = (binposit - quakeloc) #CONVERT (LAT, LON) TO (KM, KM) BEFORE FINDING NORM
-#==============================================================================
+        
+        diff = binposit - quakeloc #CONVERT (LAT, LON) TO (KM, KM) BEFORE FINDING NORM
+        diff = [[convert(pairs) for pairs in _] for _ in diff]
         rad = normalize(binposit - quakeloc, axis=2)
         
         Nom = 10**(b*(mag-delm-mc))    
@@ -36,10 +41,24 @@ def omori(quakes_f, binposit, histog, q=1.5, delm=1.0, mc=5.5, b=1.0, lam=1.76):
         chi = r0**(1-q)/Nom/(q-1)
         dist = 1.0/chi/(r0 + rad)**q/(2*pi*rad)
         
+        #==============================================================================
+        # dist = 2**(1-q)*(q-1)*10**((b+q*0.5-0.5)*mag+(1-q)*lam-b*delm-b*mc)*(0.5*10**(0.5*mag-lam)+rad)**-q
+        #==============================================================================
+        
         histog += dist
     return None
 
-def dict_filler(quakes_f):
+
+def deg2km(degs):
+    rE = 6371.0
+    latkm = math.pi/180.0*rE*degs[0]
+    lonkm = math.pi/180.0*rE*np.cos(math.pi/180.0*degs[0])*degs[1]
+    kms = (latkm, lonkm)
+    return kms
+    
+    
+def dict_filler(save_loc = "/home/jmwilson/Desktop/RELM/cats", quakes_f):
+    cat_rec = np.load('../cats/cat_rec_Feb-05-2015.p')
     hist_dict_f = {}
     flt = float
     intgr = int
@@ -51,47 +70,44 @@ def dict_filler(quakes_f):
             hist_dict_f[ind] = [quake]
         else:
             hist_dict_f[ind].append(quake)
+            
+            
+    f = open(save_loc + "/opt/hist/hist_dict_omori.p", "w")
+    pickle.dump(cat_dict, f)
+    f.close()
+    
     return hist_dict_f
 
 #==============================================================================
 # Omori distribution for forecast generation
 #   -everything is in integers until put into mesh
 #==============================================================================
-#==============================================================================
-# sim_loc = "/home/jmwilson/Desktop/RELM/sim_output"
-# sim_rec = np.load(file_loc+'/sim_rec.p')
-# 
-# edge = 50
-# lonmin = -12493
-# lonmax = -11457
-# latmin = 3156
-# latmax = 4220
-# 
-# lonlist = np.arange(lonmin-edge, lonmax+edge+1, 1)
-# latlist = np.arange(latmin-edge, latmax+edge+1, 1)
-# 
-# mglon, mglat = np.meshgrid([round(x/100.0,2) for x in lonlist], [round(x/100.0,2) for x in latlist])
-# 
-# binpos = np.array([[(y, x) for x in lonlist] for y in latlist])
-# 
-# hist_grid = np.zeros((len(latlist),len(lonlist)))
-# 
-# omori(sim_rec, binpos, hist_grid, mc=1.0)
-#==============================================================================
+sim_loc = "/home/jmwilson/Desktop/RELM/sim_output"
+sim_rec = np.load(file_loc+'/sim_rec.p')
 
-#==============================================================================
-# rates_array = np.array([mglon, mglat, hist_grid])
-# rates_array.dump(file_loc+'/opt/hist/hist_grid_omori.p')
-#==============================================================================
+edge = 50
+lonmin = -12493
+lonmax = -11457
+latmin = 3156
+latmax = 4220
+
+lonlist = np.arange(lonmin-edge, lonmax+edge+1, 1)
+latlist = np.arange(latmin-edge, latmax+edge+1, 1)
+
+mglon, mglat = np.meshgrid([round(x/100.0,2) for x in lonlist], [round(x/100.0,2) for x in latlist])
+
+binpos = np.array([[(y, x) for x in lonlist] for y in latlist])
+
+hist_grid = np.zeros((len(latlist),len(lonlist)))
+
+omori(sim_rec, binpos, hist_grid, mc=1.0)
+
+rates_array = np.array([mglon, mglat, hist_grid])
+rates_array.dump(file_loc+'/opt/hist/hist_grid_omori.p')
 
 #==============================================================================
 # Just fill dictionary with ANSS events
 #==============================================================================
-cat_loc = "/home/jmwilson/Desktop/RELM/cats"
-cat_rec = np.load('../cats/cat_rec_Feb-05-2015.p')
-
-cat_dict = dict_filler(cat_rec)
-
-f = open(cat_loc + "/opt/hist/hist_dict_omori.p", "w")
-pickle.dump(cat_dict, f)
-f.close()
+#==============================================================================
+# cat_dict = dict_filler(cat_rec)
+#==============================================================================
